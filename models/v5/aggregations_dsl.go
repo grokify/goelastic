10df620c42c3a7baa21232df6149d3467d2e01b5
John Wang <johncwang@gmail.com>
2017-07-23 23:00:02 -0700

    add aggregation response parsing

--- models/v5/aggregations_dsl.go
+++ models/v5/aggregations_dsl.go
@@ -1,5 +1,9 @@
 package v5
 
+import (
+	"encoding/json"
+)
+
 // https://www.elastic.co/guide/en/elasticsearch/reference/current/_executing_aggregations.html
 
 /*
@@ -27,26 +31,102 @@ GET /bank/_search
 // AggsBody represents the body of an aggregation
 // API request.
 type AggsBody struct {
-	Size int            `json:"size"`
-	Aggs map[string]Agg `json:"aggs,omitempty"`
+	Size  int            `json:"size"`
+	Query *Query         `json:"query,omitempty"`
+	Aggs  map[string]Agg `json:"aggs,omitempty"`
 }
 
 // Agg represents one aggregation.
+// See https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-filter-aggregation.html
+// DateHistogram: https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-datehistogram-aggregation.html#search-aggregations-bucket-datehistogram-aggregation
 type Agg struct {
-	Nested *Nested        `json:"nested,omitempty"`
-	Terms  *Term          `json:"terms,omitempty"`
-	Aggs   map[string]Agg `json:"aggs,omitempty"`
+	Filter        *Filter        `json:"filter,omitempty"`
+	Nested        *Nested        `json:"nested,omitempty"`
+	Terms         *Term          `json:"terms,omitempty"`
+	Aggs          map[string]Agg `json:"aggs,omitempty"`
+	DateHistogram *DateHistogram `json:"date_histogram,omitempty"`
+}
+
+// DateHistogram represents the Elasticsearch date_histogram aggregation.
+type DateHistogram struct {
+	Field    string `json:"field,omitempty"`
+	Interval string `json:"interval,omitempty"`
 }
 
 // Nested indicates whether the aggregation key is
 // a nested key or not. It is mandatory to use a
 // nested key (e.g. a key using dot notation).
 type Nested struct {
-	Path string `json:"path,omitempty"`
+	Path      string `json:"path,omitempty"`
+	ScoreMode string `json:"score_mode,omitempty"`
+	Query     *Query `json:"query,omitempty"`
 }
 
 // Term is the field being processed.
 type Term struct {
 	Field string `json:"field,omitempty"`
 	Null  bool   `json:"null,omitempty"`
+}
+
+// AggResponseWIP is working response object used to
+// format an aggregations response using Elastirad's
+// AggResponseRad struct.
+type AggResponseWIP struct {
+	Aggregations map[string]map[string]interface{} `json:"aggregations,omitempty"`
+}
+
+// AggregationResRad is a Go optimized response struct.
+type AggregationResRad struct {
+	AggregationName string      `json:"aggregation_name,omitempty"`
+	Type            string      `json:"type,omitempty"`
+	DocCount        int         `json:"doc_count,omitempty"`
+	AggregationData Aggregation `json:"aggregation_data,omitempty"`
+}
+
+// Aggregation is a Elasticsearch aggregation response struct.
+type Aggregation struct {
+	DocCountErrorUpperBound int      `json:"doc_count_error_upper_bound,omitempty"`
+	SumOtherDocCount        int      `json:"sum_other_doc_count,omitempty"`
+	Buckets                 []Bucket `json:"buckets,omitempty"`
+}
+
+// Bucket is an Elasticsearch aggregation response bucket struct.
+type Bucket struct {
+	Key         interface{} `json:"key,omitempty"`
+	KeyAsString string      `json:"key_as_string,omitempty"`
+	DocCount    int         `json:"doc_count,omitempty"`
+}
+
+// AggregationResRadArrayFromBodyBytes returns an array of formatted
+// Aggregations using AggregationResRad structs given a HTTP response byte
+// array.
+func AggregationResRadArrayFromBodyBytes(bytes []byte) ([]AggregationResRad, error) {
+	esRes := AggResponseWIP{}
+	esAggs := []AggregationResRad{}
+	err := json.Unmarshal(bytes, &esRes)
+	if err != nil {
+		return esAggs, err
+	}
+	for k1, v1 := range esRes.Aggregations {
+		esAgg := AggregationResRad{Type: k1}
+		for k2, v2 := range v1 {
+			if k2 == "doc_count" {
+				esAgg.DocCount = int(v2.(float64))
+			} else {
+				esAgg.AggregationName = k2
+				aggregationJsonBytes, err := json.Marshal(v2)
+				if err != nil {
+					return esAggs, err
+				}
+				agg := Aggregation{}
+				err = json.Unmarshal(aggregationJsonBytes, &agg)
+				if err != nil {
+					return esAggs, err
+				}
+				esAgg.AggregationData = agg
+			}
+		}
+		esAggs = append(esAggs, esAgg)
+	}
+	return esAggs, nil
 }
--- models/v5/query_dsl.go
+++ models/v5/query_dsl.go
@@ -1,13 +1,26 @@
 package v5
 
+// QueryBody represents a HTTP request body for the Elasticsearch
+// query API.
 type QueryBody struct {
 	Query Query `json:"query,omitempty"`
 }
 
+// Query represents a Query API query object.
 type Query struct {
-	Bool BoolQuery `json:"bool,omitempty"`
+	Bool   *BoolQuery            `json:"bool,omitempty"`
+	Nested *Nested               `json:"nested,omitempty"`
+	Match  map[string]MatchQuery `json:"match,omitempty"`
 }
 
+// MatchQuery represents a Query API match object.
+type MatchQuery struct {
+	Query          string `json:"query,omitempty"`
+	Operator       string `json:"operator,omitempty"`
+	ZeroTermsQuery string `json:"zero_terms_query,omitempty"`
+}
+
+// BoolQuery represents a Query API bool object.
 type BoolQuery struct {
 	Must               []Filter `json:"must,omitempty"`
 	Should             []Filter `json:"should,omitempty"`
@@ -15,15 +28,18 @@ type BoolQuery struct {
 	Boost              float64  `json:"boost,omitempty"`
 }
 
+// Filtered represents a Query API filtered object.
 type Filtered struct {
 	Or FilteredOr `json:"or,omitempty"`
 }
 
+// FilteredOr represents a Query API filtered or object.
 type FilteredOr struct {
 	Filters []Filter `json:"filters,omitempty"`
 	Cache   bool     `json:"_cache,qromitempty"`
 }
 
+// Filter represents a Query API filter object.
 type Filter struct {
 	Match map[string]string `json:"match,omitempty"`
 	Term  map[string]string `json:"term,omitempty"`
